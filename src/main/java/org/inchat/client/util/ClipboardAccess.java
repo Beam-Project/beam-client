22218bab305c1b55ad294976cee00b4596389c20
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-03-08 17:30:04 +0100

    Created a simple class to access the system clipboard.

---
+++ src/main/java/org/inchat/client/util/ClipboardAccess.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2013, 2014 inchat.org
+ *
+ * This file is part of inchat-client.
+ *
+ * inchat-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * inchat-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.inchat.client.util;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import org.inchat.common.util.Exceptions;
+
+/**
+ * Allows to access the local system clipboard easily.
+ */
+public class ClipboardAccess implements ClipboardOwner {
+
+    /**
+     * Copies the given text to the clipboard.
+     *
+     * @param content The text to copy. This may not be null.
+     * @throws IllegalArgumentException If the argument is null.
+     */
+    public void copyTextToClipboard(String content) {
+        Exceptions.verifyArgumentNotNull(content);
+
+        StringSelection stringSelection = new StringSelection(content);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, stringSelection);
+        clipboard.setContents(stringSelection, this);
+    }
+
+    @Override
+    public void lostOwnership(Clipboard clipboard, Transferable contents) {
+        // Do nothing.
+    }
+
+}
