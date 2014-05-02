e9ccfe30e702c28ac67c7c7ca33c726cc87262c2
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-05-03 00:49:07 +0200

    Added a connector task to connect and disconnect to respectively from a server.

---
+++ src/main/java/org/beamproject/client/ConnectorTask.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2013, 2014 beamproject.org
+ *
+ * This file is part of beam-common.
+ *
+ * beam-common is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beam-common is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.client;
+
+import org.beamproject.common.network.MessageSender;
+import java.net.MalformedURLException;
+import java.net.URL;
+import static org.beamproject.client.App.getConfig;
+import static org.beamproject.client.App.getController;
+import static org.beamproject.client.App.getModel;
+import org.beamproject.client.ui.Dialogs;
+import org.beamproject.client.ui.MainWindow;
+import org.beamproject.common.Message;
+import org.beamproject.common.Participant;
+import org.beamproject.common.Session;
+import org.beamproject.common.crypto.Handshake;
+import org.beamproject.common.crypto.HandshakeChallenger;
+import org.beamproject.common.util.Executor;
+import org.beamproject.common.util.Task;
+
+/**
+ * Allows to establish or invalidate a connection within this and another
+ * {@link Participant}.
+ * <p>
+ * If the connection is created, a new {@link Session} is negotiated. Otherwise,
+ * an existing {@link Session} will be invalidated.
+ *
+ * @see Executor
+ */
+public class ConnectorTask implements Task {
+
+    Controller controller;
+    Model model;
+    MainWindow window;
+    URL serverUrl;
+    String serverUrlAsString;
+    Participant user, server;
+    HandshakeChallenger challenger;
+    MessageSender sender;
+    Message challenge, response, success;
+    boolean doConnect;
+
+    /**
+     * Creates a connector to establish a authenticated connection to a other
+     * participant.
+     *
+     * @param doConnect If true, the connection should be established, otherwise
+     * it should be terminated.
+     */
+    public ConnectorTask(boolean doConnect) {
+        this.doConnect = doConnect;
+    }
+
+    @Override
+    public void run() {
+        prepareEnvironment();
+
+        if (controller.isServerUrlAvailable(serverUrlAsString) && server != null) {
+            prepareConnection();
+
+            if (doConnect) {
+                prepareHandshake();
+                executeHandshake();
+            } else {
+                disconnect();
+            }
+
+            updateUserInterface();
+        } else {
+            Dialogs.showConfigurationIncomplete();
+        }
+
+        window.enableStatusButton();
+    }
+
+    void prepareEnvironment() {
+        controller = getController();
+        window = controller.getMainWindow();
+        window.disableStatusButton();
+        model = getModel();
+        user = model.getUser();
+        server = model.getServer();
+        serverUrlAsString = getConfig().serverUrl();
+    }
+
+    void prepareConnection() {
+        serverUrl = getServerUrl(serverUrlAsString + getConfig().serverAuthenticationPath());
+        sender = new MessageSender(serverUrl, user);
+    }
+
+    void prepareHandshake() {
+        challenger = new HandshakeChallenger(model.getUser());
+    }
+
+    URL getServerUrl(String serverUrl) {
+        try {
+            return new URL(serverUrl);
+        } catch (MalformedURLException ex) {
+            throw new IllegalStateException("The given URL is not valid: " + ex.getMessage());
+        }
+    }
+
+    void executeHandshake() {
+        challenge = challenger.produceChallenge(server);
+        response = sender.sendAndReceive(challenge);
+        challenger.consumeResponse(response);
+        success = challenger.produceSuccess();
+        sender.send(success);
+        controller.session = new Session(server, challenger.getSessionKey());
+    }
+
+    void disconnect() {
+        Session session = controller.getSession();
+
+        if (session != null) {
+            Message invalidateMessage = Handshake.getInvalidate(server, session.getKey());
+            sender.send(invalidateMessage);
+            session.invalidateSession();
+            controller.setSession(null);
+        }
+    }
+
+    void updateUserInterface() {
+        window.labelStatusButtonAsConnected(doConnect);
+    }
+}
---
+++ src/test/java/org/beamproject/client/ConnectorTaskTest.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2013, 2014 beamproject.org
+ *
+ * This file is part of beam-client.
+ *
+ * beam-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beam-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.client;
+
+import java.net.URL;
+import static org.beamproject.client.App.getConfig;
+import org.beamproject.client.ui.MainWindow;
+import org.beamproject.common.Message;
+import org.beamproject.common.Participant;
+import org.beamproject.common.Session;
+import org.beamproject.common.crypto.HandshakeChallenger;
+import org.beamproject.common.network.MessageSender;
+import org.easymock.EasyMock;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class ConnectorTaskTest {
+
+    private final String SERVER_URL_AS_STRING = "http://srv.beamproject.org";
+    private ConnectorTask task;
+    private Controller controller;
+    private Model model;
+    private MainWindow window;
+    private Participant user, server;
+
+    @Before
+    public void setUp() {
+        ConfigTest.loadDefaultConfig();
+        user = Participant.generate();
+        server = Participant.generate();
+
+        controller = new Controller();
+        model = new Model();
+        model.user = user;
+        model.server = server;
+        window = createMock(MainWindow.class);
+        controller.mainWindow = window;
+        AppTest.setAppController(controller);
+        AppTest.setAppModel(model);
+
+        task = new ConnectorTask(true);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertTrue(task.doConnect);
+        task = new ConnectorTask(false);
+        assertFalse(task.doConnect);
+    }
+
+    @Test
+    public void testPrepareEnvironment() {
+        getConfig().setProperty("serverUrl", SERVER_URL_AS_STRING);
+
+        assertNull(task.controller);
+        assertNull(task.window);
+        assertNull(task.model);
+        assertNull(task.server);
+        assertNull(task.serverUrlAsString);
+
+        task.prepareEnvironment();
+
+        assertSame(controller, task.controller);
+        assertSame(window, task.window);
+        assertSame(model, task.model);
+        assertSame(server, task.server);
+        assertEquals(SERVER_URL_AS_STRING, task.serverUrlAsString);
+    }
+
+    @Test
+    public void testPrepareConnection() {
+        task.user = user;
+        task.serverUrlAsString = SERVER_URL_AS_STRING;
+        task.prepareConnection();
+        assertEquals(task.serverUrl.toString(), SERVER_URL_AS_STRING + getConfig().serverAuthenticationPath());
+        assertNotNull(task.sender);
+    }
+
+    @Test
+    public void testPrepareHandshake() {
+        task.model = model;
+        assertNull(task.challenger);
+        task.prepareHandshake();
+        assertNotNull(task.challenger);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGetServerUrlOnNull() {
+        task.getServerUrl(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGetServerUrlOnInvalidString() {
+        task.getServerUrl("hallo");
+    }
+
+    @Test
+    public void testGetServerUrl() {
+        URL url = task.getServerUrl(SERVER_URL_AS_STRING);
+        assertEquals(SERVER_URL_AS_STRING, url.toString());
+    }
+
+    @Test
+    public void testExecuteHandshake() {
+        task.controller = controller;
+        task.server = server;
+
+        Message challenge = createMock(Message.class);
+        Message response = createMock(Message.class);
+        Message success = createMock(Message.class);
+
+        task.challenger = createMock(HandshakeChallenger.class);
+        task.sender = createMock(MessageSender.class);
+
+        expect(task.challenger.produceChallenge(server)).andReturn(challenge);
+        expect(task.sender.sendAndReceive(challenge)).andReturn(response);
+        task.challenger.consumeResponse(response);
+        expectLastCall();
+        expect(task.challenger.produceSuccess()).andReturn(success);
+        task.sender.send(success);
+        expectLastCall();
+        expect(task.challenger.getSessionKey()).andReturn("key".getBytes());
+        replay(task.challenger, task.sender);
+
+        task.executeHandshake();
+
+        assertArrayEquals("key".getBytes(), controller.getSession().getKey());
+        assertSame(server, controller.getSession().getRemoteParticipant());
+        verify(task.challenger, task.sender);
+    }
+
+    @Test
+    public void testDisconnectOnNull() {
+        task.controller = controller;
+        task.disconnect(); // Nothing should happen when the session is null.
+    }
+
+    @Test
+    public void testDisconnect() {
+        Session session = new Session(server, "key".getBytes());
+        controller.session = session;
+        task.controller = controller;
+        task.server = server;
+
+        task.sender = createMock(MessageSender.class);
+        task.sender.send(EasyMock.anyObject(Message.class));
+        expectLastCall();
+        replay(task.sender);
+
+        task.disconnect();
+
+        assertNull(controller.session);
+        assertNull(session.getRemoteParticipant());
+
+        for (byte b : session.getKey()) {
+            assertEquals((byte) 0, b);
+        }
+
+        verify(task.sender);
+    }
+
+    @Test
+    public void testUpdateUserInterface() {
+        task.window = window;
+        window.labelStatusButtonAsConnected(true);
+        expectLastCall();
+        window.labelStatusButtonAsConnected(false);
+        expectLastCall();
+        replay(window);
+
+        task.doConnect = true;
+        task.updateUserInterface();
+
+        task.doConnect = false;
+        task.updateUserInterface();
+
+        verify(window);
+    }
+}
